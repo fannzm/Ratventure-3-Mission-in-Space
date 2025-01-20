3b90005b0fb4d5200bebdc37f36fb1d92f51ed60
fannzm <fanny.mozes@gmail.com>
2025-01-20 16:14:15 +0100

    adding PlayerName input

---
+++ Unity Ratventure 3/Assets/Scripts/PlayerNameInput.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    public TMP_InputField playerNameInputField; // Das InputField für den Spielernamen
+    public Button playButton; // Der Play-Button
+
+    private void Start()
+    {
+        // Button zunächst deaktivieren
+        playButton.interactable = false;
+
+        // InputField überwachen
+        playerNameInputField.onValueChanged.AddListener(OnNameInputChanged);
+    }
+
+    private void OnNameInputChanged(string input)
+    {
+        // Button aktivieren, wenn das InputField nicht leer ist
+        playButton.interactable = !string.IsNullOrEmpty(input);
+    }
+
+    public void OnPlayButtonClicked()
+    {
+        // Spielernamen speichern
+        string playerName = playerNameInputField.text;
+        PlayerPrefs.SetString("PlayerName", playerName);
+        Debug.Log(name);
+
+        // Spiel starten (z. B. neue Szene laden)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);// Ersetze "GameScene" mit dem Namen deiner Spielszene
+    }
+    
+    
+
+}
